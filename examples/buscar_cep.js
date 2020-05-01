d750d73bccf0757cbb4e9118f2a457f1a3701e09
Renato <renatoelysiqueira@gmail.com>
2020-05-01 14:17:15 -0300

    v2

--- examples/buscar_cep.js
+++ examples/buscar_cep.js
@@ -1,16 +1,5 @@
-var sigep = require('../index')
+var { consultaCEP } = require('../index')
 
-sigep('dev')
-  .then(function(sigepClient){
-    sigepClient
-      .consultaCEP('37552171')
-      .then(function(endereco){
-        console.log(endereco)
-      })
-      .catch(function(err){
-        console.log(err)
-      })
-  })
-  .catch(function(err){
-    console.log(err)
-  })
+consultaCEP('dev', '37552171')
+  .then(address => console.log(address))
+  .catch(err => console.log(err))
